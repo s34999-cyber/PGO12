9b3eea709b23b36fd938f3ad6286536dc3b8667c
Vincent Radecki <s34999@pjwstk.edu.pl>
2026-05-26 11:24:00 +0200

    Intialize

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
