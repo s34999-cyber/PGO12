224bc718fecb01286ebf59f4dc56fdeed93cd932
Vincent Radecki <s34999@pjwstk.edu.pl>
2026-05-29 21:02:03 +0200

    Task 8

--- src/StreamApiTasks.java
+++ src/StreamApiTasks.java
@@ -109,8 +109,12 @@ public class StreamApiTasks {
     }
 
     static Map<String, Double> revenueByCategory(List<Order> orders) {
-        // TODO: task 8
-        return Map.of();
+        return orders.stream()
+                .filter(order -> order.status() != OrderStatus.CANCELLED)
+                .flatMap(order -> order.items().stream())
+                .collect(Collectors.groupingBy(
+                        item -> item.product().category(),
+                        Collectors.summingDouble(OrderItem::totalPrice)));
     }
 
     static Map<String, Double> topCustomers(List<Order> orders, int limit) {
