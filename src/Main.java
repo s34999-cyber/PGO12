eacdd5f361f5251494760286aeb4fd8c8afb1fd3
Vincent Radecki <s34999@pjwstk.edu.pl>
2026-06-01 19:58:22 +0200

    Main message

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Wrong file! StreamApiTasks is also in this rep.");
 
 
     }
